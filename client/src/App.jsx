126bcd1685110897921ebd42373833cf1cd7d7ad
Jess Palacios <jepal10@gmail.com>
2023-01-30 09:16:06 -0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+import { logo } from './assets'
+import { Home, CreatePost } from './pages'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+        <Link
+          to="/create-post"
+          className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+        >
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Create
+          </span>
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc (100vh - 73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className='text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
+          >
+            Surprise ME
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#464FF] outline-none block w-full p-3 "
+      ></input>
+    </div>
+  )
+}
+
+export default FormField
